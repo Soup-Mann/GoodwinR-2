1eeb2c2eec164b9a0df899403a1fb30ab3a1c656
Reed Goodwin <rsgoodwin0@gmail.com>
2022-10-18 15:38:51 -0700

    AddItUp

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿//Reed Goodwin
+//10-18-2022
+//Mini Challenge #2 "add 2 Numbers"
+//A program to add numbers!
+//Peer Review 
+
+Console.Clear();
+string userInput;
+string userInput2;
+bool isconverted = true;
+bool isNumber;
+bool isNumber2;
+long validNum = 0;
+
+
+string fullstop = "";
+while(isconverted){
+    while(fullstop != "no"){
+        Console.WriteLine("Please enter your first number!");
+        userInput = Console.ReadLine();
+        isNumber = Int64.TryParse(userInput, out validNum);
+        Console.WriteLine();
+        Console.WriteLine("Please enter your second number!");
+        userInput2 = Console.ReadLine();
+        isNumber2 = Int64.TryParse(userInput2, out validNum);
+        Console.WriteLine();
+        Console.WriteLine();
+        if(isNumber == true && isNumber2 == true){
+            isconverted = false;
+            int numone = Convert.ToInt32(userInput);
+            int numtwo = Convert.ToInt32(userInput2);
+            Console.WriteLine("Your sum is..." + (numone + numtwo));
+        }else{
+            Console.WriteLine("Invalid entry, Please Try again!");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("---------");
+        Console.WriteLine("Do you want to play again?");
+        Console.WriteLine("Press enter to restart || Type \"no\" to exit! :)");
+        fullstop = fullstop.ToUpper();
+        fullstop = Console.ReadLine();
+    }
+
+
+
+
+
+
+}
